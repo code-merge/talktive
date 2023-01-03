435a49c83b67f7f3bc653cfdf50ef5da94bd53dc
Chinmay Gokhale <codewithchinu@gmail.com>
2023-01-03 20:24:48 +0530

    'fixed styling of About section'

--- App/src/js/frontend/components/About.js
+++ App/src/js/frontend/components/About.js
@@ -1,24 +1,37 @@
 import React from "react";
-import "../../../resources/styles/AboutStyle.scss";
+import { useSelector } from "react-redux";
+
 import BackButton from "./shared/Buttons/BackButton";
 
-function About({ theme }) {
-  const imgName = `talktive_logo_${theme ? "dark" : "light"}.png`;
+import "../../../resources/styles/AuthStyle.scss";
+import "../../../resources/styles/AboutStyle.scss";
+
+function About() {
+  const { isDarkTheme } = useSelector(({ settings }) => settings);
+  const imgName = `talktive_logo_${isDarkTheme ? "dark" : "light"}.png`;
   const logoImg = require(`../../../resources/images/${imgName}`);
 
   return (
     <div className="centered-view">
       <div className="centered-container">
-        <BackButton theme={theme} />
-        <div className={`centered-container-form ${theme ? "dark" : "light"}`}>
+        <BackButton theme={isDarkTheme} />
+        <div
+          className={`centered-container-form ${
+            isDarkTheme ? "dark" : "light"
+          }`}
+        >
           <div className="image-div">
             <img src={logoImg} alt="app-logo" className="logo-img"></img>
           </div>
-          <div className="about-details-container">
+          <div
+            className={`about-details-container ${
+              isDarkTheme ? "dark" : "light"
+            }`}
+          >
             <h3>TALKATIVE</h3>
             <h5>Version 1.0.0</h5>
           </div>
-          <div className="developer-info">
+          <div className={`developer-info ${isDarkTheme ? "dark" : "light"}`}>
             <span>Developer: &#169; 2022 Chinmay Gokhale</span>
           </div>
         </div>
